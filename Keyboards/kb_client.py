e144c8edeee4b8832bcff7ef1e04a5c7fc349476
Javlon1 <muhamedmijotot@gmail.com>
2023-08-30 20:26:35 +0300

    ewqw3

--- Keyboards/kb_client.py
+++ Keyboards/kb_client.py
@@ -1,50 +1,86 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-
-# btn1 = KeyboardButton('/help')
-# btn2 = KeyboardButton('/address')
-btn_contact = KeyboardButton('поделиться номером', request_contact=True)
-btn_address = KeyboardButton('отправить где я', request_location=True)
-
-
-year_btn1 = KeyboardButton(1999)
-year_btn2 = KeyboardButton(1999)
-year_btn3 = KeyboardButton(1999)
-year_btn4 = KeyboardButton(1999)
-year_btn5 = KeyboardButton(1999)
-year_btn6 = KeyboardButton(1999)
-
-
-address_btn1 = KeyboardButton(text='андижон1')
-address_btn2 = KeyboardButton(text='андижон2')
-address_btn3 = KeyboardButton(text='андижон3')
-address_btn4 = KeyboardButton(text='андижон4')
-address_btn5 = KeyboardButton(text='андижон5')
-address_btn6 = KeyboardButton(text='андижон6')
-address_btn7 = KeyboardButton(text='андижон7')
-address_btn8 = KeyboardButton(text='андижон8')
-address_btn9 = KeyboardButton(text='андижон9')
-address_btn10 = KeyboardButton(text='андижон10')
+# row, add, insert
+inline_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='оператор(жен)')
+        ],
+        [
+            KeyboardButton(text='стирка(муж)')
+        ],
+        [
+            KeyboardButton(text='доставщик')
+        ],
+    ],
+    resize_keyboard=True, one_time_keyboard=True
+)
 
 
-inkb1 = KeyboardButton(text='оператор(жен)')
-inkb2 = KeyboardButton(text='стирка(муж)')
-inkb3 = KeyboardButton(text='доставщик')
+year_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(1998),
+            KeyboardButton(1999),
+        ],
+        [
+            KeyboardButton(2000),
+            KeyboardButton(2001),
+        ],
+        [
+            KeyboardButton(2002),
+            KeyboardButton(2003),
+        ],
+    ],
+    resize_keyboard=True, one_time_keyboard=True
+)
 
 
-inline_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-inline_kb.add(inkb1).add(inkb2).add(inkb3)
+user_info_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton('поделиться номером', request_contact=True),
+            KeyboardButton('отправить где я', request_location=True)
+        ],
+    ],
+    resize_keyboard=True, one_time_keyboard=True
+)
 
 
-year_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-year_kb.row(year_btn1,year_btn2).row(year_btn3,year_btn4).row(year_btn5,year_btn6)
-
-
-contact_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-contact_kb.add(btn_contact)
-
-
-address_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-address_kb.row(address_btn1,address_btn2).row(address_btn3,address_btn4).row(address_btn5,address_btn6).row(address_btn7,address_btn8).row(address_btn9,address_btn10)
-
-#row, add, insert
+address_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Андижон Шахар'),
+            KeyboardButton(text='Андижон Туман'),
+        ],
+        [
+            KeyboardButton(text='Хонабод'),
+            KeyboardButton(text='Асака'),
+        ],
+        [
+            KeyboardButton(text='Баликчи'),
+            KeyboardButton(text='Бостон'),
+        ],
+        [
+            KeyboardButton(text='Булокбоши'),
+            KeyboardButton(text='Жалакудук'),
+        ],
+        [
+            KeyboardButton(text='Избоскан'),
+            KeyboardButton(text='Улугнор'),
+        ],
+        [
+            KeyboardButton(text='Олитнкол'),
+            KeyboardButton(text='Мархамат'),
+        ],
+        [
+            KeyboardButton(text='Пахтаобод'),
+            KeyboardButton(text='Шахрихон'),
+        ],
+        [
+            KeyboardButton(text='Хожабод'),
+            KeyboardButton(text='Коргонтепа'),
+        ],
+    ],
+    resize_keyboard=True, one_time_keyboard=True
+)
--- main.py
+++ main.py
@@ -18,11 +18,3 @@ client.register_handlers_client(dp)
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
 
-
-["""
-('message_id', 1787) 
-('from', {'id': 727134704, 'is_bot': False, 'first_name': 'Javlon', 'username': 'Muhammadjonov_javlon', 'language_code': 'ru'}) 
-('chat', {'id': 727134704, 'first_name': 'Javlon', 'username': 'Muhammadjonov_javlon', 'type': 'private'})
-('date', 1693337108) 
-('text', 'dasdas')
-"""]
