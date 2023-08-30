c50415d7127d1f3179aec6d5de23c495f1bcaaf2
Javlon1 <muhamedmijotot@gmail.com>
2023-08-30 20:34:27 +0300

    ewqw3

--- Keyboards/kb_admin.py
+++ Keyboards/kb_admin.py
@@ -1,11 +1,13 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-btn_load = KeyboardButton('/menu')
 btn_delete = KeyboardButton('/delete')
 
-
-kb_admin = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-
-
-kb_admin.add(btn_load)
+kb_admin = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton('/menu'),
+        ],
+    ],
+    resize_keyboard=True, one_time_keyboard=True
+)
